9e8bf07d554569f68e4acb9d72c6f23cf5c33598
chrisgsk <christofer.skjaeveland@gmail.com>
2019-03-08 09:53:58 +0100

    Fjerne network_module og implementer i com

--- com.go
+++ com.go
@@ -1,10 +1,55 @@
+// Network module for go
+
 //sende heartbeats
-
-//generere en random ID til seg selv
-
 //phoenix backup
+// Hele tiden oppdatere de andre heisene på sine egne orders
+// - hele tiden sende ned til orders en komplett ordreliste
+// - ta imot orders sin ordrelsite og sende ut
 
-//hele tiden oppdatere de andre heisene på sine egne orders
-//   -hele tiden sende ned til orders en komplett ordreliste
-//   -ta imot orders sin ordreliste og sende ut
+package main
 
+import (
+	"./network/bcast"
+	//"./network/localip"
+	//"./network/peers"
+	//"flag"
+	"fmt"
+	//"os"
+	"time"	
+)
+
+type MessageStruct struct {
+	Message string
+}
+
+func main() {
+	transmitMessage := make(chan MessageStruct)
+	receiveMessage := make(chan MessageStruct)
+
+	go bcast.Transmitter(16569, transmitMessage)
+	go bcast.Receiver(16569, receiveMessage)
+
+	go func() {
+		helloMsg := MessageStruct{"Hello from me"}
+		for {
+			receiveMessage <- helloMsg
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	fmt.Println("Started")
+	for {
+		select {
+		case a := <-receiveMessage:
+			fmt.Printf("Received: %#v\n", a)
+		}
+	}
+}
+
+//func UDP_init()
+
+
+//func sendMessage(adresse, beskjed)
+
+
+//func recieveMessage(addresse, beskjed)
