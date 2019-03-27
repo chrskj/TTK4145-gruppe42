60a2ce0db194802024f1cf67d3cb408274af0ad6
Martin Aalby Svalesen <martiasv@stud.ntnu.no>
2019-03-27 13:24:39 +0100

    nå skal det være gucci

--- code/elevAlgo/elevAlgo.go
+++ code/elevAlgo/elevAlgo.go
@@ -61,13 +61,6 @@ func ElevStateMachine(OrdersToElevAlgo, ElevAlgoToOrders, ComToElevAlgo,
 	for {
 		ElevatorPrinter(elevator)
 		select {
-		case a := <-OrdersToElevAlgo: //recieves a new ordre from orders
-			fmt.Printf("Entering OrdersToElevAlgo\n Setting order\n")
-			SetOrder(a.Direction, int(a.Floor), elevatorPtr)
-			//sette igang hvis samme etasje
-			if a.Floor == elevator.Floor {
-				go func() { drv_floors <- int(a.Floor) }()
-			}
 
 		case a := <-ComToElevAlgo:
 
@@ -76,13 +69,10 @@ func ElevStateMachine(OrdersToElevAlgo, ElevAlgoToOrders, ComToElevAlgo,
 			case "newOrder":
 				fmt.Printf("Got new order from comm\n")
 				fmt.Println(a)
-				if a.Direction {
-					elevator.OrdersQueue[a.Floor][ButtonUp] = true
-					SetButtonLamp(BT_HallUp, int(a.Floor), true)
-				} else {
-					elevator.OrdersQueue[a.Floor][ButtonDown] = true
-					SetButtonLamp(BT_HallDown, int(a.Floor), true)
+				if a.Floor == elevator.Floor {
+					go func() { drv_floors <- int(a.Floor) }()
 				}
+				SetOrder(a.Direction, int(a.Floor), elevatorPtr)
 				if elevator.State == Idle {
 					elevator.Dir = QueueFuncChooseDirection(elevator)
 					if elevator.Dir == DirDown {
