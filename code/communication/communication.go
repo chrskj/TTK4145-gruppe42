1830062e11ee328d0922b9bfb622c6dbdd1738ec
Blackladder <blackladder0@gmail.com>
2019-03-28 21:29:38 +0100

    added packet loss handling

--- code/communication/communication.go
+++ code/communication/communication.go
@@ -30,44 +30,94 @@ func InitCom(toElevAlgo, toOrders, fromElevAlgo, fromOrders chan ChannelPacket,
 	peerUpdateCh := make(chan peers.PeerUpdate)
 	go peers.Receiver(16569, peerUpdateCh)
 
+	handShakeChan := make(chan ChannelPacket)
+	lastRecieved := ChannelPacket{ //Dr.Frankenstein's FrankenPacket
+		orderList: []ChannelPacket{ChannelPacket{PacketType: "newOrder",Floor: 0,},
+			ChannelPacket{PacketType: "orderList", Floor: 1,},
+			ChannelPacket{PacketType: "getOrderList",Floor: 2,},
+			ChannelPacket{PacketType: "cost",Floor: 3,},
+			ChannelPacket{PacketType: "orderComplete",Floor: 4,},
+			ChannelPacket{PacketType: "requestCostFunc",Floor: 5,}}
+	}
+
 	for {
 		select {
 		case msg := <-fromElevAlgo:
 			msg.Elevator = elevID
-			sendMessage <- msg
+			SendImportantMsg(msg, sendMessage, handShakeChan)
 		case msg := <-fromOrders:
-			switch msg.PacketType {
-			case "requestCostFunc":
-				sendMessage <- msg
-			case "getOrderList":
-				sendMessage <- msg
-			case "newOrder":
-				sendMessage <- msg
-			case "orderList":
-				sendMessage <- msg
-			}
+			SendImportantMsg(msg, sendMessage, handShakeChan)
 		case msg := <-receiveMessage:
 			//fmt.Printf("Comm Recieved packet of type %s from broadcast\n", msg.PacketType)
 			switch msg.PacketType {
 			case "newOrder":
-				toOrders <- msg
-				if msg.Elevator != elevID {
-					toElevAlgo <- msg
-				}
-			case "orderList":
-				if msg.Elevator == elevID {
+				if(lastRecieved.OrderList[0].TimeStamp != msg.TimeStamp){
+					lastRecieved.OrderList[0].TimeStamp = msg.TimeStamp
+					//start
 					toOrders <- msg
+					if msg.Elevator != elevID {
+						toElevAlgo <- msg
+					}
+					//end
 				}
+				msg.PacketType = "handShake"
+				msg.Elevator = elevID
+				sendMessage <- msg
+			case "orderList":
+				if(lastRecieved.OrderList[1].TimeStamp != msg.TimeStamp){
+					lastRecieved.OrderList[1].TimeStamp = msg.TimeStamp
+					//start
+					if msg.Elevator == elevID {
+						toOrders <- msg
+					}
+					//end
+				}
+				msg.PacketType = "handShake"
+				msg.Elevator = elevID
+				sendMessage <- msg
 			case "getOrderList":
-				toOrders <- msg
+				if(lastRecieved.OrderList[2].TimeStamp != msg.TimeStamp){
+					lastRecieved.OrderList[2].TimeStamp = msg.TimeStamp
+					//start
+					toOrders <- msg
+					//end
+				}
+				msg.PacketType = "handShake"
+				msg.Elevator = elevID
+				sendMessage <- msg
 			case "cost":
-				toOrders <- msg
+				if(lastRecieved.OrderList[3].TimeStamp != msg.TimeStamp){
+					lastRecieved.OrderList[3].TimeStamp = msg.TimeStamp
+					//start
+					toOrders <- msg
+					//end
+				}
+				msg.PacketType = "handShake"
+				msg.Elevator = elevID
+				sendMessage <- msg
 			case "orderComplete":
-				toOrders <- msg
-				
-				toElevAlgo <- msg
+				if(lastRecieved.OrderList[4].TimeStamp != msg.TimeStamp){
+					lastRecieved.OrderList[4].TimeStamp = msg.TimeStamp
+					//start
+					toOrders <- msg
+					toElevAlgo <- msg
+					//end
+				}
+				msg.PacketType = "handShake"
+				msg.Elevator = elevID
+				sendMessage <- msg
 			case "requestCostFunc":
-				toElevAlgo <- msg
+				if(lastRecieved.OrderList[5].TimeStamp != msg.TimeStamp){
+					lastRecieved.OrderList[5].TimeStamp = msg.TimeStamp
+					//start
+					toElevAlgo <- msg
+					//end
+				}
+				msg.PacketType = "handShake"
+				msg.Elevator = elevID
+				sendMessage <- msg
+			case "handShake":
+				handShakeChan <- msg
 			}
 		case msg := <-peerUpdateCh:
 			fmt.Printf("Peer update:\n")
@@ -82,6 +132,33 @@ func InitCom(toElevAlgo, toOrders, fromElevAlgo, fromOrders chan ChannelPacket,
 				}
 			}
 		default:
+		}
+	}
+}
+
+func SendImportantMsg(msg ChannelPacket, sendMessage, handShakeChan chan ChannelPacket){
+	recievedHandShakes := []int{}
+	sendMessage <- msg
+	for tries := 0; tries<10 && len(recievedHandShakes)<NumElevators{
+		select{
+		case temp <- handShakeChan:
+			if temp.TimeStamp == msg.TimeStamp{
+				unique := true
+				for _, val in recievedHandShakes{
+					if temp.Elevator == val.Elevator{unique = false}
+				}
+				if unique {
+					recievedHandShakes = append(recievedHandShakes, temp)
+				}
+			} else {
+				handShakeChan <- temp
+				tries++
+				time.Sleep(300*time.Millisecond)
+			}
+		default:
+			sendMessage <- msg
+			tries++
+			time.Sleep(300*time.Millisecond)
 		}
 	}
 }
