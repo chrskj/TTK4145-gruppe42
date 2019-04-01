5db90535e790acb89fc36b5e85a2f8e0d6840fc0
chrisgsk <christofer.skjaeveland@gmail.com>
2019-04-01 11:18:52 +0200

    fix code quality of communication module

--- code/communication/communication.go
+++ code/communication/communication.go
@@ -10,8 +10,15 @@ import (
 	. "../util"
 )
 
-func InitCom(toElevAlgo, toOrders, fromElevAlgo, fromOrders chan ChannelPacket,
-	elevID int) {
+func RedundantBcast(msg ChannelPacket, sendMessage chan ChannelPacket) {
+	for tries := 0; tries < 5; tries++ {
+		sendMessage <- msg
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func InitCom(ComToElevAlgo, ComToOrders, ElevAlgoToCom,
+	OrdersToCom chan ChannelPacket, elevID int) {
 
 	sendMessage := make(chan ChannelPacket)
 	go bcast.Transmitter(16570, sendMessage)
@@ -25,85 +32,69 @@ func InitCom(toElevAlgo, toOrders, fromElevAlgo, fromOrders chan ChannelPacket,
 	peerUpdateCh := make(chan peers.PeerUpdate)
 	go peers.Receiver(16569, peerUpdateCh)
 
-	go func() {
-		msg := <-peerUpdateCh
-		for {
-			msg = <-peerUpdateCh
-			fmt.Printf("Peer update:\n")
-			fmt.Printf("  Peers:    %q\n", msg.Peers)
-			fmt.Printf("  New:      %q\n", msg.New)
-			fmt.Printf("  Lost:     %q\n", msg.Lost)
-			if len(msg.Lost) > 0 {
-				idLost, _ := strconv.Atoi(msg.Lost[0])
-				toOrders <- ChannelPacket{
-					PacketType: "elevLost",
-					Elevator:   idLost,
-				}
-			}
-		}
-	}()
-
 	lastRecieved := []uint64{0, 0, 0, 0, 0, 0}
 
 	for {
 		select {
-		case msg := <-fromElevAlgo:
-			msg.Elevator = elevID
-			go SendImportantMsg(msg, sendMessage)
-		case msg := <-fromOrders:
-			fmt.Println("From orders!")
-			go SendImportantMsg(msg, sendMessage)
+		case msg := <-ElevAlgoToCom:
+			go RedundantBcast(msg, sendMessage)
+		case msg := <-OrdersToCom:
+			go RedundantBcast(msg, sendMessage)
 		case msg := <-receiveMessage:
 			switch msg.PacketType {
 			case "newOrder":
 				if lastRecieved[0] != msg.Timestamp {
 					lastRecieved[0] = msg.Timestamp
-					toOrders <- msg
+					ComToOrders <- msg
 					if msg.Elevator != elevID {
-						toElevAlgo <- msg
+						ComToElevAlgo <- msg
 					}
 				}
 			case "orderList":
 				if lastRecieved[1] != msg.Timestamp {
 					lastRecieved[1] = msg.Timestamp
 					if msg.Elevator == elevID {
-						toOrders <- msg
+						ComToOrders <- msg
 					}
 				}
 			case "getOrderList":
 				if lastRecieved[2] != msg.Timestamp {
 					lastRecieved[2] = msg.Timestamp
-					toOrders <- msg
+					ComToOrders <- msg
 
 				}
 			case "cost":
 				if lastRecieved[3] != msg.Timestamp {
 					lastRecieved[3] = msg.Timestamp
-					toOrders <- msg
+					ComToOrders <- msg
 				}
 
 			case "orderComplete":
 				if lastRecieved[4] != msg.Timestamp {
 					lastRecieved[4] = msg.Timestamp
-					toOrders <- msg
-					toElevAlgo <- msg
+					ComToOrders <- msg
+					ComToElevAlgo <- msg
 				}
 
 			case "requestCostFunc":
 				if lastRecieved[5] != msg.Timestamp {
 					lastRecieved[5] = msg.Timestamp
-					toElevAlgo <- msg
+					ComToElevAlgo <- msg
 				}
 			}
-
+		case msg := <-peerUpdateCh:
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", msg.Peers)
+			fmt.Printf("  New:      %q\n", msg.New)
+			fmt.Printf("  Lost:     %q\n", msg.Lost)
+			if len(msg.Lost) > 0 {
+				idLost, _ := strconv.Atoi(msg.Lost[0])
+				ComToOrders <- ChannelPacket{
+					PacketType: "elevLost",
+					Elevator:   idLost,
+				}
+			}
 		default:
 		}
-	}
-}
-
-func SendImportantMsg(msg ChannelPacket, sendMessage chan ChannelPacket) {
-	for tries := 0; tries < 5; tries++ {
-		sendMessage <- msg
-		time.Sleep(100 * time.Millisecond)
 	}
 }
